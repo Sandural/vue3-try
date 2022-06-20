c3c9245489bfea152eaa56e219c7491033f4cbc1
rick <1370365840@qq.com>
2022-06-20 10:31:02 +0800

    表格fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/styles/index.scss'
-import { router } from './router'
+import { router } from './router/index'
 
 const app = createApp(App)
 app.use(router)
